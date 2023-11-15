a7734dc6e21d2966580b29d1e86354070a5d93bd
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2023-11-15 02:13:08 -0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -147,4 +147,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'notificaciondepaginaweb@gmail.com'
-EMAIL_HOST_PASSWORD = 'iddaknkbspmdhanl'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
